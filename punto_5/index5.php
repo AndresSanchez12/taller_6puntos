814e0e750bd404f7527f9c011fbb1fdbecf16558
CristianFernandez25 <cdfernandez25@gmail.com>
2025-03-22 09:46:21 -0500

    Correcciones

---
+++ punto_5/index5.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css5.css">
+    <title>Conversor a Binario</title>
+   
+</head>
+<body>
+    <h2>Convertir Número a Binario</h2>
+    <form action="punto_5.php" method="POST">
+        <input type="number" name="numero" placeholder="Ingrese un número entero" required>
+        <button type="submit">Convertir</button>
+    </form>
+    
+   
+</body>
+</html>
