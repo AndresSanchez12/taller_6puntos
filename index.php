519f0bd78f5f1961174f9ea5bea96a1ebda884b1
Tu Nombre <tuemail@example.com>
2025-03-22 08:55:17 -0500

    menu

--- index.php
+++ index.php
@@ -3,11 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
     <title>Document</title>
 </head>
 <body>
     <section>
-        
+        <h1>MENU PRINCIPAL</h1><br>
+
+        <a href="">1. Convertir una frase en su acrónimo.</a><br><br>
+        <a href="">2. Calcular Sucesión de Fibonacci o el factorial</a><br><br>
+        <a href="">3. Calcular el promedio, la media y la moda</a><br><br>
+        <a href="">4. Calcular la unión, intersección y diferencia de dos conjuntos</a><br><br>
+        <a href="">5. Convertir un número entero en binario.</a><br><br>
+        <a href="">6. Construir un árbol binario según los recorridos preorden, inorden y postorden.</a><br><br>
     </section>
     
 </body>
