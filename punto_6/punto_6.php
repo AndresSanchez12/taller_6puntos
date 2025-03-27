11d4a76a55e102c7e961d4ee91cd9ec836a02cc9
CristianFernandez25 <cdfernandez25@gmail.com>
2025-03-26 19:49:18 -0500

    Ultimos cambios

--- punto_6/punto_6.php
+++ punto_6/punto_6.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Construcción de Árbol Binario</title>
-    <link rel="stylesheet" href="../css.css">
+    <link rel="stylesheet" href="../punto_6/punto6css.css">
 
 </head>
 <body>
